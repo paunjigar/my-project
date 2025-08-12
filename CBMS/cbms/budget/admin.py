4e0c8658ea17070e69f3128034ad257a3abc029d
paunjigar <paunjigar9@gmail.com>
2025-08-12 19:32:09 +0530

    Delete CBMS directory

--- CBMS/cbms/budget/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Person, Expense, Income
-
-# Register your models here.
-admin.site.register(Person)
-admin.site.register(Expense)
-admin.site.register(Income)
